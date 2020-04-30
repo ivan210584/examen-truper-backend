f55c5b5c05fb0b83626e219a040307db8633d66c
Equipo <equipo@MacBook-Air-de-test.local>
2020-04-30 17:48:37 -0500

    Quitar comentarios

--- webapi_csharp/Models/UsuarioLogin.cs
+++ webapi_csharp/Models/UsuarioLogin.cs
@@ -3,7 +3,6 @@ namespace webapi_csharp.Models
 {
     public class UsuarioLogin
     {
-        //Comentario de prueba 2
         public string Usuario { get; set; }
         public string Password { get; set; }
     }
